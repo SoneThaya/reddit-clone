3d5e5ecc90fc03817e772f2e1dde6421bb20d607
sone <sthaya@gmail.com>
2022-07-18 20:55:52 -0700

    setup next auth and reddit sign in

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,8 +16,11 @@ import {
   SpeakerphoneIcon,
   VideoCameraIcon,
 } from "@heroicons/react/outline";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -61,18 +64,45 @@ const Header = () => {
         <MenuIcon className="icon" />
       </div>
 
-      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image
-            objectFit="contain"
-            src="https://links.papareact.com/23l"
-            layout="fill"
-            alt="reddit"
-          />
-        </div>
+      {/* Sign in / Sign out button */}
+      {session ? (
+        <div
+          onClick={() => signOut()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt="reddit"
+            />
+          </div>
 
-        <p className="text-gray-400">Sign In</p>
-      </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
+        </div>
+      ) : (
+        <div
+          onClick={() => signIn()}
+          className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2"
+        >
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image
+              objectFit="contain"
+              src="https://links.papareact.com/23l"
+              layout="fill"
+              alt="reddit"
+            />
+          </div>
+
+          <p className="text-gray-400">Sign In</p>
+        </div>
+      )}
     </div>
   );
 };
